4d057325c802797ba10102ebbeae85651fd386b3
Morekid <daniele.dematteis@gmail.com>
2024-07-02 18:52:41 +0200

    add new components

---
+++ components/SiteHeader.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import { Icons } from "@/components/icons";
+import { ThemeToggle } from "@/components/theme-toggle";
+import { buttonVariants } from "@/components/ui/button";
+import { siteConfig } from "@/config/site";
+import { cn } from "@/lib/utils";
+import { NavItem } from "@/types/nav";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import styles from "./SiteHeader.module.scss";
+
+export function SiteHeader() {
+  const nav: NavItem[] = [...siteConfig.mainNav];
+  nav.shift();
+
+  const pathname = usePathname();
+
+  return (
+    <header
+      className={styles.SiteHeader + " sticky top-0 z-40 w-full bg-background"}
+    >
+      <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
+        <Link href="/" className={styles.Logo}>
+          <Image
+            className={styles.Image + " dark"}
+            src="/static/brand/LogoLockupExploreDark+Title.svg"
+            alt="Arcjet Example app"
+            height={30}
+            width={310}
+          />
+          <Image
+            className={styles.Image + " light"}
+            src="/static/brand/LogoLockupExploreLight+Title.svg"
+            alt="Arcjet Example app"
+            height={30}
+            width={310}
+          />
+        </Link>
+
+        <div className="flex flex-1 items-center justify-end space-x-4">
+          {nav?.length ? (
+            <nav className="flex gap-5 mt-[5px]">
+              {nav?.map((item, index) => {
+                const selected = pathname.indexOf(item.key) >= 0;
+
+                return item.href && selected ? (
+                  <span
+                    key={index}
+                    className={cn(
+                      "flex items-center px-1 text-md font-bold text-primary ",
+                    )}
+                  >
+                    {item.title}
+                  </span>
+                ) : (
+                  <Link
+                    key={index}
+                    href={item.href || ""}
+                    className={buttonVariants({
+                      size: "smTight",
+                      variant: "linkMuted",
+                    })}
+                  >
+                    {item.title}
+                  </Link>
+                );
+              })}
+            </nav>
+          ) : null}
+
+          <nav className="flex items-center space-x-1 mt-[5px]">
+            <Link
+              href="https://github.com/arcjet/arcjet-js-example"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div
+                className={cn(
+                  buttonVariants({
+                    variant: "ghost",
+                  }),
+                  "w-9 px-0",
+                )}
+              >
+                <Icons.gitHub className="size-4" />
+                <span className="sr-only">GitHub</span>
+              </div>
+            </Link>
+            <ThemeToggle />
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+}
---
+++ components/compositions/WhatNext.tsx
@@ -0,0 +1,106 @@
+import { buttonVariants } from "@/components/ui/button";
+import { cva, type VariantProps } from "class-variance-authority";
+import Link from "next/link";
+import * as React from "react";
+import { memo } from "react";
+
+import styles from "./WhatNext.module.scss";
+
+export const variants = cva([styles.Comp, " grid items-center gap-6"], {
+  variants: {
+    variant: {
+      live: styles.Live,
+      deployed: styles.Deployed,
+    },
+  },
+  defaultVariants: {
+    variant: "live",
+  },
+});
+
+export interface Props
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof variants> {
+  deployed?: boolean;
+}
+
+const WhatNext = React.forwardRef<HTMLDivElement, Props>(
+  ({ className, deployed, variant, ...props }, ref) => {
+    return (
+      <div ref={ref} className={variants({ variant, className })} {...props}>
+        <h2 className="text-xl font-bold">What next?</h2>
+        <div className="flex flex-col gap-8">
+          {deployed ? (
+            <>
+              <div className="flex gap-6 items-center">
+                <Link
+                  href="https://github.com/arcjet/arcjet-js/tree/main/examples"
+                  target="_blank"
+                  className={buttonVariants({ variant: "outline" })}
+                >
+                  See all example apps
+                </Link>
+                <Link
+                  href="https://docs.arcjet.com"
+                  target="_blank"
+                  className="hover:underline font-bold decoration-1 underline-offset-2"
+                >
+                  Arcjet docs
+                </Link>
+              </div>
+              <div className="flex gap-3">
+                <span className="text-secondary-foreground">Get in touch</span>
+
+                <Link
+                  href="https://discord.gg/TPra6jqZDC"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="hover:underline font-bold decoration-1 underline-offset-2"
+                >
+                  Join our Discord
+                </Link>
+              </div>
+            </>
+          ) : (
+            <>
+              <div>
+                <Link
+                  href="https://app.arcjet.com"
+                  target="_blank"
+                  className={buttonVariants({ variant: "outline" })}
+                >
+                  Sign up for Arcjet
+                </Link>
+              </div>
+              <div className="flex gap-3">
+                <span className="text-secondary-foreground">
+                  Want to know more?
+                </span>
+
+                <Link
+                  href="https://docs.arcjet.com"
+                  target="_blank"
+                  className="hover:underline font-bold decoration-1 underline-offset-2"
+                >
+                  Arcjet docs
+                </Link>
+                <span className="text-muted-foreground">/</span>
+                <Link
+                  href="https://discord.gg/TPra6jqZDC"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="hover:underline font-bold decoration-1 underline-offset-2"
+                >
+                  Join our Discord
+                </Link>
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    );
+  },
+);
+WhatNext.displayName = "WhatNext";
+
+export default memo(WhatNext);
