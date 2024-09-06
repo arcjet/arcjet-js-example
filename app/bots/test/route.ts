57d1b842b92f593e4ac2d92a83bb15c4fdfecbba
David Mytton <david@arcjet.com>
2024-09-06 09:36:50 +0100

    chore: Update Arcjet to alpha.24 (#122)

--- app/bots/test/route.ts
+++ app/bots/test/route.ts
@@ -8,7 +8,9 @@ export const dynamic = "force-dynamic";
 const aj = arcjet.withRule(
   detectBot({
     mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
-    block: ["AUTOMATED"], // blocks all automated clients
+    // configured with a list of bots to allow from
+    // https://arcjet.com/bot-list
+    allow: [], // blocks all automated clients
   }),
 );
 
